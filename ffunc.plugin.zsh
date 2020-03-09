f9f4aa6ad52173c86563f4a64a56c2dec5ea2844
ijyo <ijyo@protonmail.ch>
2020-03-09 16:51:08 +0900

    Add git utility.

--- ffunc.plugin.zsh
+++ ffunc.plugin.zsh
@@ -1,4 +1,5 @@
 FFUNC_ROOT=${${(%):-%x}:A:h}
 
 source $FFUNC_ROOT/cd/ffunc-cd.plugin.zsh
+source $FFUNC_ROOT/git/ffunc-git.plugin.zsh
 source $FFUNC_ROOT/history/ffunc-history.plugin.zsh
---
+++ git/ffunc-git.plugin.zsh
@@ -0,0 +1,16 @@
+function ffunc::git::log() {
+  local git=${GIT:-git}
+  local grep_hash="echo {} | grep -o '[a-f0-9]\{7\}'"
+  local git_show="$grep_hash | head -1 | xargs -I% $git show --color=always %"
+  $git log --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr%" $@ | \
+    fzf --ansi \
+      --no-sort \
+      --no-multi \
+      --tiebreak=index \
+      --bind="enter:execute($git_show | LESS='-R' less)" \
+      --bind="ctrl-y:execute-silent($grep_hash | xargs -I% echo -n "%" | xsel -ib)+abort" \
+      --bind="ctrl-b:execute($grep_hash | xargs -I% git rebase -i %)+abort" \
+      --bind='?:toggle-preview' \
+      --preview-window='right:60%' \
+      --preview="$git_show"
+}
