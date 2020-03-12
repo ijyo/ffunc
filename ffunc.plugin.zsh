85788ab1fe251b3c0eb4aafa8827937531f98367
ijyo <ijyo@protonmail.ch>
2020-03-13 03:28:53 +0900

    Rename files.

--- ffunc.plugin.zsh
+++ ffunc.plugin.zsh
@@ -1,6 +1,6 @@
 FFUNC_ROOT=${${(%):-%x}:A:h}
 
-source $FFUNC_ROOT/cd/ffunc-cd.plugin.zsh
-source $FFUNC_ROOT/file/ffunc-file.plugin.zsh
-source $FFUNC_ROOT/git/ffunc-git.plugin.zsh
-source $FFUNC_ROOT/history/ffunc-history.plugin.zsh
+source $FFUNC_ROOT/fcd/fcd.plugin.zsh
+source $FFUNC_ROOT/ffile/ffile.plugin.zsh
+source $FFUNC_ROOT/fgit/fgit.plugin.zsh
+source $FFUNC_ROOT/fhistory/fhistory.plugin.zsh
