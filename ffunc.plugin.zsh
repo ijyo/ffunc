38f908202780d9b6537a4284c0d31823f22bd0a7
ijyo <ijyo@protonmail.ch>
2022-12-05 07:49:53 +0900

    Fix

--- ffunc.plugin.zsh
+++
@@ -1,6 +0,0 @@
-FFUNC_ROOT=${${(%):-%x}:A:h}
-
-source $FFUNC_ROOT/fcd/fcd.plugin.zsh
-source $FFUNC_ROOT/ffm/ffm.plugin.zsh
-source $FFUNC_ROOT/fgit/fgit.plugin.zsh
-source $FFUNC_ROOT/fhist/fhist.plugin.zsh
--- init.zsh
+++ init.zsh
@@ -1,3 +1,10 @@
+FFUNC_ROOT=${${(%):-%x}:A:h}
+
+source $FFUNC_ROOT/fcd/fcd.plugin.zsh
+source $FFUNC_ROOT/ffm/ffm.plugin.zsh
+source $FFUNC_ROOT/fgit/fgit.plugin.zsh
+source $FFUNC_ROOT/fhist/fhist.plugin.zsh
+
 # fcd
 alias c=fcd::all
 
